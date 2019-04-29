a0894aa4b6fa760b1c634e89130cb7d5361bdf97
ShowJs <tt5118@hotmail.com>
2019-04-29 16:18:59 +0900

    About Props.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React from 'react';
 
 function App() {
+  const profiles = [
+    { name: 'Taro', age: 30 },
+    { name: 'Yooooo', age: 3 },
+    { name: 'Woooooo' }
+  ];
+
   return (
-    <input
-      type="text"
-      onClick={() => {
-        console.log('1234');
-      }}
-    />
+    <div>
+      {profiles.map((profile, index) => {
+        return <User name={profile.name} age={profile.age} key={index} />;
+      })}
+    </div>
   );
 }
+
+const User = props => {
+  return (
+    <div>
+      My name is {props.name}, and {props.age} years old.
+    </div>
+  );
+};
+
+User.defaultProps = {
+  age: 50
+};
 
 export default App;
